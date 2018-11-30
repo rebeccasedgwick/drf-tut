06f057b845e2bfce6abd25454cfb9e4e1df65163
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-11-30 22:53:16 +0000

    Adds permissions: restrict creation / editing of snippets

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,24 +1,26 @@
 from django.contrib.auth.models import User
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 
 class SnippetList(generics.ListCreateAPIView):
     """
     List all snippets, or create a new snippet.
     """
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request_user)
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update, or delete a snippet instance.
     """
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 

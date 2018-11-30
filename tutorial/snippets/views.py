2d58495099792ff4045a9d1e0569b1163f00f442
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-11-30 22:18:18 +0000

    Adds users / associates with snippets / updates db models

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.models import User
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
 
 
@@ -10,6 +11,9 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request_user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
@@ -17,3 +21,13 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer

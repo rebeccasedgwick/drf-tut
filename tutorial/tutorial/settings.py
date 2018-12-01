184176597bd822927d63912ae9835340d7e69192
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-12-01 00:21:14 +0000

    Specifies pagination

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -120,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# REST framework settings
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
